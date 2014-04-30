8b278af1e47b922c19afc652d2a754665bc4ddb9
Andy <webmaster@RealityRipple.com>
2014-04-30 11:09:52 -0700

    Initial Commit

---
+++ Please Stand By/frmMain.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.components = New System.ComponentModel.Container()
+    Me.trayStandBy = New System.Windows.Forms.NotifyIcon(Me.components)
+    Me.mnuTray = New System.Windows.Forms.ContextMenu()
+    Me.mnuStandby = New System.Windows.Forms.MenuItem()
+    Me.mnuStartUp = New System.Windows.Forms.MenuItem()
+    Me.mnuSpace = New System.Windows.Forms.MenuItem()
+    Me.mnuExit = New System.Windows.Forms.MenuItem()
+    Me.tmrHide = New System.Windows.Forms.Timer(Me.components)
+    Me.mnuShortcut = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutDisabled = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutCA = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutAS = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutCAD = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutCAM = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutCAS = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutCAV = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutW = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutWS = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutWV = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutASD = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutASM = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutASS = New System.Windows.Forms.MenuItem()
+    Me.mnuShortcutASV = New System.Windows.Forms.MenuItem()
+    Me.SuspendLayout()
+    '
+    'trayStandBy
+    '
+    Me.trayStandBy.ContextMenu = Me.mnuTray
+    Me.trayStandBy.Icon = Global.Please_Stand_By.My.Resources.Resources.icon
+    Me.trayStandBy.Text = "Left Click: Monitor Standby" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Right Click: Menu"
+    Me.trayStandBy.Visible = True
+    '
+    'mnuTray
+    '
+    Me.mnuTray.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.mnuStandby, Me.mnuShortcut, Me.mnuStartUp, Me.mnuSpace, Me.mnuExit})
+    Me.mnuTray.Name = "mnuTray"
+    
+    '
+    'mnuStandby
+    '
+    Me.mnuStandby.Name = "mnuStandby"
+    Me.mnuStandby.Text = "Enable Monitor Standby"
+    '
+    'mnuStartUp
+    '
+    Me.mnuStartUp.Name = "mnuStartUp"
+    Me.mnuStartUp.Text = "Start with Windows"
+    '
+    'mnuSpace
+    '
+    Me.mnuSpace.Name = "mnuSpace"
+    Me.mnuSpace.Text = "-"
+    '
+    'mnuExit
+    '
+    Me.mnuExit.Name = "mnuExit"
+    Me.mnuExit.Text = "Exit"
+    '
+    'tmrHide
+    '
+    Me.tmrHide.Enabled = True
+    Me.tmrHide.Interval = 10
+    '
+    'mnuShortcut
+    '
+    Me.mnuShortcut.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.mnuShortcutDisabled, Me.mnuShortcutCA, Me.mnuShortcutAS, Me.mnuShortcutW})
+    Me.mnuShortcut.Name = "mnuShortcut"
+    Me.mnuShortcut.Text = "Keyboard Shortcut"
+    '
+    'mnuShortcutDisabled
+    '
+    Me.mnuShortcutDisabled.Name = "mnuShortcutDisabled"
+    Me.mnuShortcutDisabled.Text = "Disabled"
+    '
+    'mnuShortcutCA
+    '
+    Me.mnuShortcutCA.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.mnuShortcutCAD, Me.mnuShortcutCAM, Me.mnuShortcutCAS, Me.mnuShortcutCAV})
+    Me.mnuShortcutCA.Name = "mnuShortcutCA"
+    Me.mnuShortcutCA.Text = "Ctrl+Alt"
+    '
+    'mnuShortcutAS
+    '
+    Me.mnuShortcutAS.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.mnuShortcutASD, Me.mnuShortcutASM, Me.mnuShortcutASS, Me.mnuShortcutASV})
+    Me.mnuShortcutAS.Name = "mnuShortcutAS"
+    Me.mnuShortcutAS.Text = "Alt+Shift"
+    '
+    'mnuShortcutCAD
+    '
+    Me.mnuShortcutCAD.Name = "mnuShortcutCAD"
+    Me.mnuShortcutCAD.Text = "Ctrl+Alt+D"
+    '
+    'mnuShortcutCAM
+    '
+    Me.mnuShortcutCAM.Name = "mnuShortcutCAM"
+    Me.mnuShortcutCAM.Text = "Ctrl+Alt+M"
+    '
+    'mnuShortcutCAS
+    '
+    Me.mnuShortcutCAS.Name = "mnuShortcutCAS"
+    Me.mnuShortcutCAS.Text = "Ctrl+Alt+S"
+    '
+    'mnuShortcutCAV
+    '
+    Me.mnuShortcutCAV.Name = "mnuShortcutCAV"
+    Me.mnuShortcutCAV.Text = "Ctrl+Alt+V"
+    '
+    'mnuShortcutW
+    '
+    Me.mnuShortcutW.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.mnuShortcutWS, Me.mnuShortcutWV})
+    Me.mnuShortcutW.Name = "mnuShortcutW"
+    Me.mnuShortcutW.Text = "Win"
+    '
+    'mnuShortcutWS
+    '
+    Me.mnuShortcutWS.Name = "mnuShortcutWS"
+    Me.mnuShortcutWS.Text = "Win+S"
+    '
+    'mnuShortcutWV
+    '
+    Me.mnuShortcutWV.Name = "mnuShortcutWV"
+    Me.mnuShortcutWV.Text = "Win+V"
+    '
+    'mnuShortcutASD
+    '
+    Me.mnuShortcutASD.Name = "mnuShortcutASD"
+    Me.mnuShortcutASD.Text = "Alt+Shift+D"
+    '
+    'mnuShortcutASM
+    '
+    Me.mnuShortcutASM.Name = "mnuShortcutASM"
+    Me.mnuShortcutASM.Text = "Alt+Shift+M"
+    '
+    'mnuShortcutASS
+    '
+    Me.mnuShortcutASS.Name = "mnuShortcutASS"
+    Me.mnuShortcutASS.Text = "Alt+Shift+S"
+    '
+    'mnuShortcutASV
+    '
+    Me.mnuShortcutASV.Name = "mnuShortcutASV"
+    Me.mnuShortcutASV.Text = "Alt+Shift+V"
+    '
+    'frmMain
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(232, 143)
+    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+    Me.Icon = Global.Please_Stand_By.My.Resources.Resources.icon
+    Me.Name = "frmMain"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Please Stand By..."
+    Me.ResumeLayout(False)
+
+  End Sub
+  Friend WithEvents trayStandBy As System.Windows.Forms.NotifyIcon
+  Friend WithEvents tmrHide As System.Windows.Forms.Timer
+  Friend WithEvents mnuTray As System.Windows.Forms.ContextMenu
+  Friend WithEvents mnuStandby As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuSpace As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuExit As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuStartUp As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcut As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutDisabled As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutCA As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutAS As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutCAD As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutCAM As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutCAS As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutCAV As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutASD As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutASM As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutASS As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutASV As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutW As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutWS As System.Windows.Forms.MenuItem
+  Friend WithEvents mnuShortcutWV As System.Windows.Forms.MenuItem
+
+End Class
---
+++ Please Stand By/frmMain.vb
@@ -0,0 +1,265 @@
+﻿Imports System.Runtime.InteropServices
+Imports IWshRuntimeLibrary
+Public Class frmMain
+  <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Unicode)>
+  Private Shared Function PostMessage(ByVal hWnd As IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
+  End Function
+
+  <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Unicode)>
+  Private Shared Function RegisterHotKey(ByVal hwnd As IntPtr, ByVal id As Integer, ByVal fsModifiers As Integer, ByVal vk As Integer) As Boolean
+  End Function
+
+  <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Unicode)>
+  Private Shared Function UnregisterHotKey(ByVal hwnd As IntPtr, ByVal id As Integer) As Boolean
+  End Function
+
+  Private Enum MOD_
+    Alt = &H1
+    Control = &H2
+    Shift = &H4
+    Win = &H8
+    NoRepeat = &H4000
+  End Enum
+
+  Private Sub tmrHide_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrHide.Tick
+    tmrHide.Enabled = False
+    Me.Hide()
+    If Application.UserAppDataRegistry.ValueCount > 0 Then
+      Select Case Application.UserAppDataRegistry.GetValue("KeyboardShortcut", 0)
+        Case 0
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+        Case 1
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Control Or MOD_.Alt Or MOD_.NoRepeat, Keys.D) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Ctrl+Alt+D")
+        Case 2
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Control Or MOD_.Alt Or MOD_.NoRepeat, Keys.M) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Ctrl+Alt+M")
+        Case 3
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Control Or MOD_.Alt Or MOD_.NoRepeat, Keys.S) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Ctrl+Alt+S")
+        Case 4
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Control Or MOD_.Alt Or MOD_.NoRepeat, Keys.V) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Ctrl+Alt+V")
+        Case 5
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Alt Or MOD_.Shift Or MOD_.NoRepeat, Keys.D) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Alt+Shift+D")
+        Case 6
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Alt Or MOD_.Shift Or MOD_.NoRepeat, Keys.M) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Alt+Shift+M")
+        Case 7
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Alt Or MOD_.Shift Or MOD_.NoRepeat, Keys.S) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Alt+Shift+S")
+        Case 8
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Alt Or MOD_.Shift Or MOD_.NoRepeat, Keys.V) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Alt+Shift+V")
+        Case 9
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Win Or MOD_.NoRepeat, Keys.S) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Win+S")
+        Case 10
+          If Not RegisterHotKey(Me.Handle, 1, MOD_.Win Or MOD_.NoRepeat, Keys.V) Then Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+          SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Win+V")
+      End Select
+    End If
+  End Sub
+
+  Private Sub mnuExit_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles mnuExit.Click
+    Application.Exit()
+  End Sub
+
+  Private Sub mnuStandby_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles mnuStandby.Click
+    PostMessage(Me.Handle, &H112&, &HF170&, 2)
+  End Sub
+
+  Private Sub trayStandBy_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles trayStandBy.MouseUp
+    If e.Button = Windows.Forms.MouseButtons.Left Then PostMessage(Me.Handle, &H112&, &HF170&, 2)
+  End Sub
+
+  Public Sub CreateShortCut()
+    Dim shell As New WshShell
+    Dim shortCut As IWshRuntimeLibrary.IWshShortcut
+    shortCut = CType(shell.CreateShortcut(My.Computer.FileSystem.SpecialDirectories.Programs & "\Startup\Please Stand By.lnk"), IWshRuntimeLibrary.IWshShortcut)
+    With shortCut
+      .TargetPath = Application.ExecutablePath
+      .WindowStyle = 1
+      .Description = "Please Stand By..."
+      .WorkingDirectory = Application.StartupPath
+      .IconLocation = Application.ExecutablePath & ",0"
+      .Save()
+    End With
+  End Sub
+
+  Private Sub mnuStartUp_Click(sender As Object, e As System.EventArgs) Handles mnuStartUp.Click
+    If Not My.Computer.FileSystem.FileExists(My.Computer.FileSystem.SpecialDirectories.Programs & "\Startup\Please Stand By.lnk") Then
+      CreateShortCut()
+    Else
+      If My.Computer.FileSystem.FileExists(My.Computer.FileSystem.SpecialDirectories.Programs & "\Startup\Please Stand By.lnk") Then My.Computer.FileSystem.DeleteFile(My.Computer.FileSystem.SpecialDirectories.Programs & "\Startup\Please Stand By.lnk")
+    End If
+  End Sub
+
+  Private Sub mnuTray_Popup(sender As System.Object, e As EventArgs) Handles mnuTray.Popup
+    mnuStartUp.Checked = My.Computer.FileSystem.FileExists(My.Computer.FileSystem.SpecialDirectories.Programs & "\Startup\Please Stand By.lnk")
+    mnuShortcutDisabled.Checked = False
+    mnuShortcutASD.Checked = False
+    mnuShortcutASM.Checked = False
+    mnuShortcutASS.Checked = False
+    mnuShortcutASV.Checked = False
+    mnuShortcutCAD.Checked = False
+    mnuShortcutCAM.Checked = False
+    mnuShortcutCAS.Checked = False
+    mnuShortcutCAV.Checked = False
+    mnuShortcutWS.Checked = False
+    mnuShortcutWV.Checked = False
+    If Application.UserAppDataRegistry.ValueCount > 0 Then
+      Select Case Application.UserAppDataRegistry.GetValue("KeyboardShortcut", 0)
+        Case 0 : mnuShortcutDisabled.Checked = True
+        Case 1 : mnuShortcutCAD.Checked = True
+        Case 2 : mnuShortcutCAM.Checked = True
+        Case 3 : mnuShortcutCAS.Checked = True
+        Case 4 : mnuShortcutCAV.Checked = True
+        Case 5 : mnuShortcutASD.Checked = True
+        Case 6 : mnuShortcutASM.Checked = True
+        Case 7 : mnuShortcutASS.Checked = True
+        Case 8 : mnuShortcutASV.Checked = True
+        Case 9 : mnuShortcutWS.Checked = True
+        Case 10 : mnuShortcutWV.Checked = True
+      End Select
+    Else
+      mnuShortcutDisabled.Checked = True
+    End If
+  End Sub
+
+  Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+    If m.Msg = &H312 Then
+      If m.WParam.ToInt32 = 1 Then
+        PostMessage(Me.Handle, &H112&, &HF170&, 2)
+      End If
+    End If
+    MyBase.WndProc(m)
+  End Sub
+
+  Private Sub mnuShortcutDisabled_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutDisabled.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+  End Sub
+
+  Private Sub mnuShortcutCAD_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutCAD.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 1)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Ctrl+Alt+D")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Control Or MOD_.Alt Or MOD_.NoRepeat, Keys.D) Then
+      MsgBox("The Keyboard Shortcut ""Ctrl+Alt+D"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub mnuShortcutCAM_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutCAM.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 2)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Ctrl+Alt+M")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Control Or MOD_.Alt Or MOD_.NoRepeat, Keys.M) Then
+      MsgBox("The Keyboard Shortcut ""Ctrl+Alt+M"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub mnuShortcutCAS_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutCAS.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 3)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Ctrl+Alt+S")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Control Or MOD_.Alt Or MOD_.NoRepeat, Keys.S) Then
+      MsgBox("The Keyboard Shortcut ""Ctrl+Alt+S"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub mnuShortcutCAV_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutCAV.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 4)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Ctrl+Alt+V")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Control Or MOD_.Alt Or MOD_.NoRepeat, Keys.V) Then
+      MsgBox("The Keyboard Shortcut ""Ctrl+Alt+V"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub mnuShortcutASD_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutASD.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 5)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Alt+Shift+D")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Alt Or MOD_.Shift Or MOD_.NoRepeat, Keys.D) Then
+      MsgBox("The Keyboard Shortcut ""Alt+Shift+D"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub mnuShortcutASM_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutASM.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 6)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Alt+Shift+M")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Alt Or MOD_.Shift Or MOD_.NoRepeat, Keys.M) Then
+      MsgBox("The Keyboard Shortcut ""Alt+Shift+M"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub mnuShortcutASS_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutASS.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 7)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Alt+Shift+S")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Alt Or MOD_.Shift Or MOD_.NoRepeat, Keys.S) Then
+      MsgBox("The Keyboard Shortcut ""Alt+Shift+S"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub mnuShortcutASV_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutASV.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 8)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Alt+Shift+V")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Alt Or MOD_.Shift Or MOD_.NoRepeat, Keys.V) Then
+      MsgBox("The Keyboard Shortcut ""Alt+Shift+V"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub mnuShortcutWS_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutWS.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 9)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Win+S")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Win Or MOD_.NoRepeat, Keys.S) Then
+      MsgBox("The Keyboard Shortcut ""Win+S"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub mnuShortcutWV_Click(sender As System.Object, e As System.EventArgs) Handles mnuShortcutWV.Click
+    Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 10)
+    UnregisterHotKey(Me.Handle, 1)
+    SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu" & vbNewLine & "Standby Shortcut: Win+V")
+    If Not RegisterHotKey(Me.Handle, 1, MOD_.Win Or MOD_.NoRepeat, Keys.V) Then
+      MsgBox("The Keyboard Shortcut ""Win+V"" is already in use!" & vbNewLine & "Please select a different Keyboard Shortcut to use with Please Stand By...", MsgBoxStyle.Exclamation Or MsgBoxStyle.SystemModal, "Please Stand By... Keyboard Shortcut")
+      Application.UserAppDataRegistry.SetValue("KeyboardShortcut", 0)
+      SetNotifyIconText(trayStandBy, "Left Click: Monitor Standby" & vbNewLine & "Right Click: Menu")
+    End If
+  End Sub
+
+  Private Sub SetNotifyIconText(ni As NotifyIcon, text As String)
+    If text.Length >= 128 Then text = text.Substring(0, 124) & "..."
+    Dim t As Type = GetType(NotifyIcon)
+    Dim hidden As Reflection.BindingFlags = Reflection.BindingFlags.NonPublic Or Reflection.BindingFlags.Instance
+    t.GetField("text", hidden).SetValue(ni, text)
+    If CBool(t.GetField("added", hidden).GetValue(ni)) Then
+      t.GetMethod("UpdateIcon", hidden).Invoke(ni, New Object() {True})
+    End If
+  End Sub
+End Class
