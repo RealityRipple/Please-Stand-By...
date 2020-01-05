0e599d6e1649776c9b22b3fb536971df230a6fcb
Andy <webmaster@RealityRipple.com>
2020-01-05 13:13:58 -0800

    Require Signed EXE

--- Please Stand By/My Project/AssemblyInfo.vb
+++ Please Stand By/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("18adebe0-e3c3-4835-b82d-8bf7252d6a16")> 
+<Assembly: Guid("18adebe0-e3c3-4835-b82d-8bf7252d6a16")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.0.0")> 
-<Assembly: AssemblyFileVersion("1.2.0.0")> 
+<Assembly: AssemblyVersion("1.3.0.0")>
+<Assembly: AssemblyFileVersion("1.3.0.0")>
