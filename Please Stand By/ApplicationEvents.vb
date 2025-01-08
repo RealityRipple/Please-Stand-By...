28d8a0cfc3bdcac61665b8a7a744a50233c18176
Andy <webmaster@RealityRipple.com>
2025-01-07 17:00:53 -0800

    Manual Single Instancing using Mutex and Pipe
    .NET's built-in SingleInstance system seems to have major issues.

--- Please Stand By/ApplicationEvents.vb
+++ Please Stand By/ApplicationEvents.vb
@@ -1,12 +1,14 @@
 ﻿Namespace My
-  ' The following events are available for MyApplication:
-  ' Startup: Raised when the application starts, before the startup form is created.
-  ' Shutdown: Raised after all application forms are closed.  This event is not raised if the application terminates abnormally.
-  ' UnhandledException: Raised if the application encounters an unhandled exception.
-  ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
-  ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
   Partial Friend Class MyApplication
+    Private WithEvents sia As SingleInstance
     Private Sub MyApplication_Startup(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) Handles Me.Startup
+      sia = New SingleInstance()
+      If Not sia.NewMutex() Then
+        sia.Send(e.CommandLine)
+        e.Cancel = True
+        Return
+      End If
+      sia.NewPipe()
       Dim v As Authenticode.Validity = Authenticode.IsSelfSigned(Reflection.Assembly.GetExecutingAssembly().Location)
       If Not (v = Authenticode.Validity.SignedAndValid Or v = Authenticode.Validity.SignedButUntrusted) Then
         Dim sErr As String = "0x" & v.ToString("x")
@@ -16,18 +18,105 @@
           Return
         End If
       End If
-      If e.CommandLine.Contains("/uninstall") Then
-        cSettings.RemoveAll()
-        e.Cancel = True
-        Return
+      If e.CommandLine IsNot Nothing AndAlso e.CommandLine.Count > 0 Then
+        If e.CommandLine.Contains("/uninstall") OrElse e.CommandLine.Contains("-uninstall") Then
+          cSettings.RemoveAll()
+          e.Cancel = True
+          Return
+        End If
       End If
     End Sub
-    Private Sub MyApplication_StartupNextInstance(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs) Handles Me.StartupNextInstance
-      If e.CommandLine.Contains("/uninstall") Then
-        cSettings.RemoveAll()
-        System.Windows.Forms.Application.Exit()
-        Return
+    Private Sub sia_NewInstanceStartup(sender As Object, e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs) Handles sia.NewInstanceStartup
+      If e.CommandLine IsNot Nothing AndAlso e.CommandLine.Count > 0 Then
+        If e.CommandLine.Contains("/uninstall") OrElse e.CommandLine.Contains("-uninstall") Then
+          cSettings.RemoveAll()
+          System.Windows.Forms.Application.Exit()
+          Return
+        End If
       End If
+    End Sub
+  End Class
+  Friend Class SingleInstance
+    Public Event NewInstanceStartup(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs)
+    Private mutex As Threading.Mutex
+    Private pipe As IO.Pipes.NamedPipeServerStream
+    Private MutexID As String = Nothing
+    Private MainForm As String = Nothing
+    Public Sub New(Optional ByVal sMain As String = Nothing)
+      mutex = Nothing
+      pipe = Nothing
+      MutexID = System.Runtime.InteropServices.Marshal.GetTypeLibGuidForAssembly(System.Reflection.Assembly.GetExecutingAssembly).ToString("D")
+      MainForm = sMain
+    End Sub
+    Public Function NewMutex() As Boolean
+      Try
+        Dim isNew As Boolean = True
+        mutex = New Threading.Mutex(True, MutexID, isNew)
+        Return isNew
+      Catch ex As Exception
+        Return False
+      End Try
+    End Function
+    Public Function NewPipe() As Boolean
+      Try
+        If pipe IsNot Nothing Then
+          pipe.Close()
+          pipe.Dispose()
+          pipe = Nothing
+        End If
+        pipe = New IO.Pipes.NamedPipeServerStream(MutexID, IO.Pipes.PipeDirection.In, IO.Pipes.NamedPipeServerStream.MaxAllowedServerInstances, IO.Pipes.PipeTransmissionMode.Message, IO.Pipes.PipeOptions.Asynchronous Or IO.Pipes.PipeOptions.WriteThrough)
+        pipe.BeginWaitForConnection(AddressOf Connected, Nothing)
+        Return True
+      Catch ex As Exception
+        Return False
+      End Try
+    End Function
+    Public Function Send(ByVal CommandLine As Collections.ObjectModel.ReadOnlyCollection(Of String)) As Boolean
+      Try
+        Using sendPipe As New IO.Pipes.NamedPipeClientStream(".", MutexID, IO.Pipes.PipeDirection.Out, IO.Pipes.PipeOptions.Asynchronous Or IO.Pipes.PipeOptions.WriteThrough, Security.Principal.TokenImpersonationLevel.Anonymous)
+          sendPipe.Connect(2000)
+          If Not sendPipe.IsConnected Then Return False
+          Using wStream As New IO.StreamWriter(sendPipe)
+            wStream.WriteLine(My.Application.Info.ProductName & " - " & MutexID)
+            wStream.WriteLine(CommandLine.Count)
+            For Each n As String In CommandLine
+              wStream.WriteLine(n)
+            Next
+          End Using
+          sendPipe.Close()
+          Return True
+        End Using
+      Catch ex As Exception
+        Return False
+      End Try
+    End Function
+    Private Sub Connected(ByVal ar As IAsyncResult)
+      Try
+        pipe.EndWaitForConnection(ar)
+        Dim CommandLine As New List(Of String)
+        Using rStream As New IO.StreamReader(pipe)
+          If Not rStream.ReadLine() = My.Application.Info.ProductName & " - " & MutexID Then Return
+          Dim sLen As String = rStream.ReadLine()
+          Dim iLen As Integer = CInt(sLen)
+          Do Until rStream.EndOfStream
+            CommandLine.Add(rStream.ReadLine())
+          Loop
+          If Not iLen = CommandLine.Count Then CommandLine = New List(Of String)
+        End Using
+        NewPipe()
+        Dim e As New Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs(CommandLine.AsReadOnly, False)
+        RaiseEvent NewInstanceStartup(Me, e)
+        If e.BringToForeground Then
+          For Each frm As Form In Application.OpenForms
+            If Not frm.Name = MainForm Then Continue For
+            If Not frm.Visible Then frm.Show()
+            frm.BringToFront()
+            frm.Activate()
+            Exit For
+          Next
+        End If
+      Catch ex As Exception
+      End Try
     End Sub
   End Class
 End Namespace
